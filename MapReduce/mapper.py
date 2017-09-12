5e3c6d2fe244563e9f1380f6d1df330672fac307
Padam Sethia <padamsethia5@gmail.com>
2017-09-13 02:55:11 +0530

    Added Mapper for Mapreduce

---
+++ MapReduce/mapper.py
@@ -0,0 +1,33 @@
+import sys 
+def mapper():
+
+	# Read input line
+	for line in sys.stdin:
+		# Strip off whitespace , and split on tab
+		data = line.strip().split('\t')
+
+		# We'll use the data stored in data/mapreduce/
+		# Having six columns of tab seperated values
+		# We'll make sure that correct data in sent through 
+		print data
+		if len(data) == 6:
+			date , time , store , category ,cost , payment = data 
+
+			# Now we'll print our data as required for the reducer task
+			# I need category and cost so we'll print that
+			print "{0}/t{1}".format(category , cost)
+
+test_data = """2012-01-01\t09:00\tSan Jose\tMen's Clothing\t214.05\tAmex
+	2012-01-01\t09:00\tFort Worth\tWomen's Clothing\t153.57\tVisa
+	2012-01-01\t09:00\tSan Diego\tMusic\t66.08\tCash
+	2012-01-01\t09:00\tPittsburgh\tPet Supplies\t493.51\tDiscover
+	2012-01-01\t09:00\tOmaha\tChildren's Clothing\t235.63\tMasterCard"""
+
+def main():
+
+	# Used for testing the mapper function
+
+	import StringIO
+	sys.stdin = StringIO.StringIO(test_data)
+	mapper()
+	sys.stdin = sys.__stdin__
